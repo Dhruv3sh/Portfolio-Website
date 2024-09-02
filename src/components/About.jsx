44915139284b71559cdabf170e382d1f87bd7254
Dhruv Sharma <112899026+Dhruv3sh@users.noreply.github.com>
2024-09-02 13:38:34 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Ui from '../assets/UI.gif'
 
 const About = () => {
   return (
     
     <div className=' flex flex-row justify-center items-center w-full h-screen bg-gradient-to-b from-gray-900 via-gray-900 to-black text-white'>
 
-      <div name="about" className='flex flex-col justify-center items-center w-3/5 h-screen bg-gradient-to-b from-gray-900 via-gray-900 to-black text-white'>
-        <div className='max-w-screen-lg p-0 mx-auto flex flex-col justify-center w-auto h-full'>
+      <div name="about" className='flex flex-row justify-center items-center w-full h-screen bg-gradient-to-b from-gray-900 via-gray-900 to-black text-white'>
+        <div className=' p-0 mx-auto flex flex-col justify-center w-3/5 h-full'>
           <div className='pb-5 pl-10'>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
               About
@@ -19,7 +20,7 @@ const About = () => {
             <br />
 
             <p className='text-xl'>
-              I focus on both front-end development and back-end development, striving to create engaging and user-friendly interfaces. My skills include: HTML, CSS, Tailwind CSS, JavaScript, ReactJs, C and MYSQL.
+              I focus on both front-end development and back-end development, striving to create engaging and user-friendly interfaces. My skills include: HTML, CSS, Tailwind CSS, JavaScript, ReactJs, C++ and MYSQL.
             </p>
 
             <br />
@@ -31,11 +32,11 @@ const About = () => {
 
             </p>
           </div>
+          
         </div>
-      </div>
-      <div className='text-white w-1/4 pl-20' >
-
-        
+        <div className='h-96 w-96 pt-[74px] pr-6'>
+            <img className=' h-80 rounded-md shadow-md shadow-slate-900' src={Ui} alt="gif" />
+        </div>
       </div>
     </div>
   )
